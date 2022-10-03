40e94d75348c5374c6fc63a52cd1fbd950334927
markfvl <105167965+markfvl@users.noreply.github.com>
2022-10-03 16:29:04 +0200

    Modified CL argument parsing
    Flags added: all, offensive, scapy, verbose

--- pcaphunt.py
+++ pcaphunt.py
@@ -1,34 +1,43 @@
 import sys
 import os
-import pyshark 
+import argparse
+import pyshark
+import pathlib
 import nest_asyncio
 nest_asyncio.apply()
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
 from net.attacks.networkattacks import *
 from net.recon.hostdiscovery import *
+from net.offense import credentialSpoof
 
-if len(sys.argv) > 1 and sys.argv[1] == '-s':
-    from net.recon.hostdiscoveryScapy import arp_scanningScapy
 from utils.parser import *
 from utils.pcapStat import *
 import utils.menu
 
-
 ####################### CALL ATTACKS ###########################
 
 
-def callAttacks(attackTOAnalyse, filePath, stats, flag):
+def callAttacks(filePath, stats, args, attackTOAnalyse):
 
     attacks = [arpSpoofing, packet_loss, pingOfDeathIPv4, icmpFlood, 
                 syn_flood, dns_req_flood, vlan_hopping]
-    if(flag):
+    if args.scapy > 0:
+        from net.recon.hostdiscoveryScapy import arp_scanningScapy
         scans = [arp_scanningScapy, IP_protocol_scan, icmp_ping_sweeps_scan, 
                 tcp_syn_ping_sweep, tcp_ack_ping_sweep, udp_ping_scan]
     else:
         scans = [arp_scanning, IP_protocol_scan, icmp_ping_sweeps_scan, 
                 tcp_syn_ping_sweep, tcp_ack_ping_sweep, udp_ping_scan]
 
-    if(attackTOanalyse[0] == 1):
+    if(args.all > 0 or attackTOanalyse[0] == 3):
+        print("RECON: \n")
+        allScans(scans, filePath)
+        print("\nNETWORK ATTACKS: \n")
+        allAttacks(attacks, filePath, stats)
+    elif(attackTOanalyse[0] == 1):
         if(attackTOanalyse[1] == 1): #Spoof
             if(attackTOanalyse[2] == 1):
                 attacks[0](filePath)
@@ -52,12 +61,6 @@ def callAttacks(attackTOAnalyse, filePath, stats, flag):
                 scans[attackTOanalyse[2]](filePath)
             else:
                 allScans(scans, filePath)
-            
-    elif(attackTOanalyse[0] == 3):
-        print("RECON: \n")
-        allScans(scans, filePath)
-        print("\nNETWORK ATTACKS: \n")
-        allAttacks(attacks, filePath, stats)
     else:
         print("Some error has accurred, quitting...")
         sys.exit(-1)
@@ -80,35 +83,48 @@ def allScans(scans, filePath):
 
 if __name__ == "__main__":
 
-    scapee = False
-    #pcap path
-    if len(sys.argv) == 3 and os.path.isfile(sys.argv[2]):
-        filePath = sys.argv[2]
-        if(sys.argv[1] == '-s'):
-            scapee = True
-    elif len(sys.argv) == 2 and os.path.isfile(sys.argv[1]):
-        filePath = sys.argv[1]
-    elif len(sys.argv) == 2 and sys.argv[1] == '-s':
-            scapee = True
-            filePath = inputFilePath()
+    #PARSER
+    parser = argparse.ArgumentParser(prog="PcapHunt", description="program to analyze and discover possible threats in a pcap file.")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-a", "--all", help="Execute all attack scans", action="count", default=0)
+    group.add_argument("-O", "--offensive", help="Starts offensive mode (default starts all attacks)", choices=["ftp", "http", "all"], default="none")
+    parser.add_argument("filePath", metavar="pcapFile", type=pathlib.Path, help="Path to pcap file to analyze")
+    parser.add_argument("-s", "--scapy", help="Use scapy functions", action="count", default=0)
+    parser.add_argument("-v", "--verbose", help="Verbose mode (-vv for double verbose)", action="count", default=0)
+    args = parser.parse_args()
+
+    #PCAP PATH
+    if os.path.isfile(args.filePath):
+        filePath = str(args.filePath)
     else:
         filePath = inputFilePath()
-    
-    attackTOanalyse = utils.menu.menu()
-    print("\nANALYSING the pcap...\n")
-    
-    cap = pyshark.FileCapture(filePath)
-    stats = basicStat(cap)
-    callAttacks(attackTOanalyse, filePath, stats, scapee)
-    
 
-    #STATS 
-    print("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-    print("\nGeneral stats:")
-    for key, value in stats.items():
-        if(key == 'total_packets'):
-            print(f"\t{key} : {value}")
-        elif(value != 0):
-            print(f"\t{key} : {value}\t({value*100/stats['total_packets']}%)")
+    cap = pyshark.FileCapture(filePath)
+    attackTOanalyse = None
+
+    if args.all == 0 and args.offensive == "none":
+        attackTOanalyse = utils.menu.menu()
+
+    if args.offensive == "none":
+        print(Fore.BLUE + Style.BRIGHT + "\nANALYSING the pcap...\n")
+        stats = basicStat(cap)
+        print("Done. Starting scanning for attacks...\n")
+        print(Style.RESET_ALL)
+        callAttacks(filePath, stats, args, attackTOanalyse)
+    else:
+        print(Fore.RED + Style.BRIGHT + "OFFENSIVE MODE ENGAGED")
+        print(Style.BRIGHT + f"\t(trying to find {args.offensive.upper()} credentials in the pcap...)\n")
+        print(Style.RESET_ALL)
+        credentialSpoof.credentialSpoof(args.offensive, filePath)
+
+    #STATS
+    if(args.verbose > 0 and args.offensivr == "none"):
+        print("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+        print("\nGeneral stats:")
+        for key, value in stats.items():
+            if(key == 'total_packets'):
+                print(f"\t{key} : {value}")
+            elif(value != 0):
+                print(f"\t{key} : {value}\t({value*100/stats['total_packets']}%)")
     print()
 
