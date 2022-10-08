04729e15dc8200dd609817d953d1b87a0a63ec67
markfvl <105167965+markfvl@users.noreply.github.com>
2022-10-08 23:34:19 +0200

    Added ftp credentials sniffing

--- net/offense/credentialSniff.py
+++ net/offense/credentialSniff.py
@@ -1,13 +1,16 @@
+import sys
 import pyshark
 
-def credentialSpoof(protocol, filePath):
+
+def credentialSniff(protocol, filePath):
 
     if(protocol == "http"):
         httpGetCred(filePath)
     elif(protocol == "ftp"):
-        print("FTP credential spoofing has yet to be implemented")
+        ftpCred(filePath)
     elif(protocol == "all"):
-        pass
+        httpGetCred(filePath)
+        ftpCred(filePath)
     else:
         print("Some error has occurred!")
         sys.exit(-2)
@@ -47,10 +50,84 @@ def httpGetCred(filePath):
             httpCredentials["Credentials"][user] = pwd
 
     if(httpCredentials):    
-        print(f"Server IP : {httpCredentials['Server IP']}\nServer Port : {httpCredentials['Server Port']}\n")
+        print(f"HTTP server IP : {httpCredentials['Server IP']}\nHTTP server Port : {httpCredentials['Server Port']}\n")
+        print("HTTP CREDENTIALS:")
         for k, v in httpCredentials.items():
             if k == "Credentials":
                 for user, pwd in httpCredentials[k].items():
                     print(f"\t username = {user}, password = {pwd}")
     else:
         print("No HTTP credentials have been found")
+    print()
+
+def ftpCred(filePath):
+
+    ftp_filter = "tcp"
+    ftp_cap = pyshark.FileCapture(filePath, display_filter=ftp_filter)
+
+    datas = []
+    ftpCredentials = {}
+    usernames = {}
+    server_not_found = True
+    index = 0
+
+    for pkt in ftp_cap:
+        try:
+            payload = str(pkt.tcp.payload)
+            hex_user = "55:53:45:52:20"
+            hex_pass = "50:41:53:53"
+
+            if hex_user in payload:
+                if(server_not_found):
+                    ftpCredentials["serverIP"] = pkt.ip.dst
+                    ftpCredentials["serverPort"] = pkt.tcp.dstport
+                    server_not_found = False
+                    datas.append(ftpCredentials)
+                elif pkt.ip.dst != ftpCredentials["serverIP"]:
+                    for dict in datas:
+                        if pkt.ip.dst == dict["serverIP"]:
+                            #server is already in list
+                            continue
+                        else:
+                            # add server in list
+                            ftpCredentials["serverIP"] = pkt.ip.dst
+                            ftpCredentials["serverPort"] = pkt.tct.dstport
+                            datas.append(ftpCredentials)    
+                            index +=1
+                #USERNAME 
+                username_hex_column = payload[payload.find("20")+3 :-6]
+                username_hex = "".join(n for n in username_hex_column if n.isalnum())
+                user = bytes.fromhex(username_hex).decode("ascii") # got the usename in ascii
+                usernames[user] = pkt.tcp.stream #save stream and username inside a dict
+            #PWD
+            elif hex_pass in payload:
+                pwd_hex_column = payload[payload.find("53")+9 :-6]
+                pwd_hex = "".join(n for n in pwd_hex_column if n.isalnum())
+                pwd = bytes.fromhex(pwd_hex).decode("ascii") #got the password in ascii
+
+                for k,v in usernames.items():
+                    if v == pkt.tcp.stream:
+                        try:
+                            for dict in datas:
+                                if pkt.ip.dst == dict["serverIP"]:
+                                    dict["Credentials"][user] = pwd
+                            
+                        except KeyError:
+                            for dict in datas:
+                                if pkt.ip.dst == dict["serverIP"]:
+                                    dict["Credentials"] = {}
+                                    dict["Credentials"][user] = pwd
+        except AttributeError:
+            pass
+
+    if(datas):  
+        for dict in datas:
+            print(f"FTP server IP : {dict['serverIP']}\nFTP server Port : {dict['serverPort']}\n") 
+            print("FTP CREDENTIALS:")
+            for k, v in dict.items():
+                if k == "Credentials":
+                    for user, pwd in dict[k].items():
+                        print(f"\t username = {user}, password = {pwd}")
+    else:
+        print("No FTP credentials have been found")
+    print()
